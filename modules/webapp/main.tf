080c87a3ca38e3a6f7b2c51514bc3a22f106d523
Ankur Patel <32640688+ankurcloudworld@users.noreply.github.com>
2024-10-22 12:15:09 +0530

    Update main.tf

--- modules/webapp/main.tf
+++ modules/webapp/main.tf
@@ -23,7 +23,9 @@ resource "azurerm_linux_web_app" "app_service" {
     http2_enabled       = true
     use_32_bit_worker   = false
   }
-
+  application_stack {
+    node_version = "20-lts"  # Specifies the Node.js version
+  }
    app_settings = {
       "WEBSITE_NODE_DEFAULT_VERSION" = "20"  # Adjust to your desired Node.js version
     }
